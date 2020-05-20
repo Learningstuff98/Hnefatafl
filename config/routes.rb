964dae658a108e36e288ec9ca0213bb2c82b45e1
Andy Strube <andystrube64@gmail.com>
2020-05-19 17:53:15 -0700

    Started how to play page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   devise_for :users
   root 'static_pages#index'
+  get 'how_to_play', to: 'static_pages#how_to_play'
   resources :games, only: [:create, :show, :destroy, :edit, :update, :index] do
     resources :pieces, only: [:index, :update, :destroy]
   end
